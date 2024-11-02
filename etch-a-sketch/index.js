57f6b5165a7aa33b53e1d4db8921fe518f3af883
zygrade009 <vigneshcheepurapalli@gmail.com>
2024-11-02 16:32:07 +0530

    etch a sketch version -1 done

--- etch-a-sketch/index.js
+++ etch-a-sketch/index.js
@@ -1,12 +1,62 @@
+// constants size=16
+
+let size=16;
+
+
+/// function to change color
+function randomColor(){
+    let red=Math.round(Math.random()*256);
+    let green=Math.round(Math.random()*256);
+    let blue=Math.round(Math.random()*256);
+    return `rgb(${red},${green},${blue})`;    
+}
+
+
+/// function to create grid
 function createGrid() {
     const board = document.querySelector(".board");
-    const size=300/16;
-    for (let i = 0; i < (16 * 16); i++) {
+    board.innerHTML = ''; // Clear existing grid
+    const squareSize = 300/size;
+    for (let i = 0; i < (size * size); i++) {
         const div = document.createElement("div");
-        div.style.width = `${size}px`;
-        div.style.height = `${size}px`;
+        div.class=`${i}`;
+        div.style.width = `${squareSize}px`;
+        div.style.height = `${squareSize}px`;
+        div.addEventListener("mousemove", ()=>{
+            div.style.backgroundColor = randomColor();
+        });
         board.appendChild(div);
     }
 }
+
+/// reset button
+const resetButton = document.querySelector("#reset");
+function resetGrid() {
+    const board = document.querySelector(".board");
+    const divs = board.getElementsByTagName('div');
+    for (let i = 0; i < divs.length; i++) {
+        divs[i].style.backgroundColor = "white";
+    }
+}
+resetButton.addEventListener("click", resetGrid);
+
+
+///resize button
+
+const resizeButton = document.querySelector("#resize");
+function resizeGrid(){
+    const board = document.querySelector(".board");
+    input = document.querySelector("#size");
+    if(input.value>=100){
+        alert("Size should be less than 100");
+        return;
+    }else{
+        size = input.value;
+        board.innerHTML="";
+        createGrid();    
+    }
+    return;
+}
+resizeButton.addEventListener("click", resizeGrid);
 
 createGrid();
